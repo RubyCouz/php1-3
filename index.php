26092a20ae769cb8f2781573607c10d9e6e2c21f
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-25 16:02:31 +0200

    exo3

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 3</title>
+  </head>
+  <body>
+    <p>
+    <?php
+    //création de la variable km et défiition de sa valeur à 1
+    $km = 125;
+    echo $km;
+
+     ?>
+</p>
+  </body>
+</html>
